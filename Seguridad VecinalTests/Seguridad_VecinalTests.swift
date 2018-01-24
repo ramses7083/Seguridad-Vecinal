bf41370deedc306fb86b9d0bcc61d1c5e64e4a86
ramses7083 <ramses7083@gmail.com>
2018-01-24 11:51:58 -0600

    Initial Commit

---
+++ Seguridad VecinalTests/Seguridad_VecinalTests.swift
@@ -0,0 +1,36 @@
+//
+//  Seguridad_VecinalTests.swift
+//  Seguridad VecinalTests
+//
+//  Created by Ramses Miramontes Meza on 24/01/18.
+//  Copyright © 2018 Ramses Miramontes Meza. All rights reserved.
+//
+
+import XCTest
+@testable import Seguridad_Vecinal
+
+class Seguridad_VecinalTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
